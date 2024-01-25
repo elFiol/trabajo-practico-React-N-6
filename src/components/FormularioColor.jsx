448af8f09110bb83de92169f50dd650acbdf4d9f
Fiol Thiago <fiolthiago9@gmail.com>
2024-01-25 19:56:06 -0300

    añadimos la logica para agregar colores y tambien limpiar el formulario

--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -1,17 +1,25 @@
 import { Form, Button } from "react-bootstrap";
 import "./Principal.css"
+import { useEffect, useState } from "react";
 
 const FormularioColor = () => {
+    const [color, setColor] = useState("")
+    const [colores, setColores] = useState([])
+    const handlerSubmit = (e) => {
+        e.preventDefault()
+        setColores([...colores, color])
+        setColor("")
+    }
     return (
         <div className="formularioDiv">
             <h3 className="text-center mb-2">Administrador de Colores</h3>
             <div className="formularioSubDiv">
                 <div className="colorForm"></div>
-                <Form className="w-75">
+                <Form className="w-75" onSubmit={handlerSubmit}>
                     <Form.Group>
-                        <Form.Control type="text" placeholder="ingrese un color. EJ: blue"></Form.Control>
+                        <Form.Control type="text" maxLength={50} minLength={2} placeholder="ingrese un color. EJ: blue" onChange={(e) => { setColor(e.target.value) }} value={color} required></Form.Control>
                     </Form.Group>
-                    <Button className="mt-3">Guardar</Button>
+                    <Button type="submit" className="mt-3">Guardar</Button>
                 </Form>
             </div>
         </div>
